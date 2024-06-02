4c22c9324f4f21151a998190a36fd9ea20882559
Alex Alexeyev <aasmc@yandex.ru>
2024-06-02 13:05:26 +0300

    Initial Commit

---
+++ postgresql/init.sql
@@ -0,0 +1,4 @@
+CREATE DATABASE product_service_db;
+CREATE DATABASE orders_service_db;
+
+
---
+++ start-containers-old-docker.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo -e "\n*** Starting Docker Containers ***\n"
+
+docker volume rm deployment_db-data
+
+docker-compose up -d
+
+
